1f02ab9c05dfaf29e442e1d9ee0ee5634b4898f3
ErickZH <zaraterick@outlook.com>
2018-02-07 12:24:36 -0600

    Instalación de HTML Webpack plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlWebpack = new HtmlWebpackPlugin({
+	template: './assets/index.template.html',
+	filename: 'index.html'
+});
+
 module.exports = {
 	entry: './assets/javascript/entry.js',
 	output: {
@@ -7,6 +14,6 @@ module.exports = {
 		path: path.join(__dirname, '..'),
 		filename: 'dist/javascript/bundle.js'
 	},
-	plugins: [],
+	plugins: [htmlWebpack],
 	module: {}
 }
