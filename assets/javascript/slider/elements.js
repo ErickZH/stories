327952cd028826e4abbb15fd790f18a5d786f0a1
ErickZH <zaraterick@outlook.com>
2018-02-23 12:13:04 -0600

    Separación del array de elements.

---
+++ assets/javascript/slider/elements.js
@@ -0,0 +1,34 @@
+const elements = [
+	{
+		title: 'Laravel Básico',
+		subtitle: 'Laravel',
+		image: '../public/images/6.jpg',
+		text: 'Aprende a dominar el Framework Web #1 de PHP'
+	},
+	{
+		title: 'Roles y permisos en Laravel',
+		subtitle: 'Laravel',
+		image: '../public/images/5.jpg',
+		text: 'Lleva tu aplicación Laravel a otro nivel'
+	},
+	{
+		title: 'GUI con FXRuby',
+		subtitle: 'Ruby',
+		image: '../public/images/4.jpg',
+		text: 'Crea interfaces graficas en Ruby'
+	},
+	{
+		title: 'Git',
+		subtitle: 'Herramientas',
+		image: '../public/images/1.jpg',
+		text: 'Aprende el sistema de versionamiento más utilizado por los Desarrolladores'
+	},
+	{
+		title: 'Compilador en Ruby',
+		subtitle: 'Ruby',
+		image: '../public/images/2.jpg',
+		text: 'Crea un compilidor con Ruby'
+	}
+];
+
+export default elements;
--- assets/javascript/slider/sliderDOM.js
+++ assets/javascript/slider/sliderDOM.js
@@ -1,4 +1,5 @@
 import Slider from './slider';
+import elements from './elements';
 
 let sliderText = document.querySelector("#slider-text");
 let sliderTitle = document.querySelector("#slider-title");
@@ -8,38 +9,7 @@ let textContent = document.querySelector("#slider-text-content")
 
 
 let slider = new Slider({
-	elements: [
-	{
-		title: 'Title course',
-		subtitle: 'Description',
-		image: '../public/images/6.jpg',
-		text: 'Total number of videos'
-	},
-	{
-		title: 'Title course 2',
-		subtitle: 'Description 2',
-		image: '../public/images/5.jpg',
-		text: 'Total number of videos 2'
-	},
-	{
-		title: 'Title course 3',
-		subtitle: 'Description 3',
-		image: '../public/images/4.jpg',
-		text: 'Total number of videos 3'
-	},
-	{
-		title: 'Title course 4',
-		subtitle: 'Description 4',
-		image: '../public/images/1.jpg',
-		text: 'Total number of videos 4'
-	},
-	{
-		title: 'Title course 5',
-		subtitle: 'Description 5',
-		image: '../public/images/2.jpg',
-		text: 'Total number of videos 5'
-	}
-	],
+	elements,
 	animationFunc: function(element) 
 	{
 		textContent.classList.add("hide");
