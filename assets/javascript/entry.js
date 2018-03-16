c0f15596fb65463ec23378695b9384dffe2bc0fb
Erick Zarate <zaraterick@outlook.com>
2018-03-16 12:33:36 -0600

    Se implemento la funcionalidad de abrir y cerrar el menu.

--- assets/javascript/entry.js
+++ assets/javascript/entry.js
@@ -2,9 +2,12 @@ import '../css/app.scss';
 
 import './slider/sliderDOM';
 
-import './maps';
-
 document.querySelector('.menu')
 	.addEventListener('click', function() {
 		document.querySelector('.menu-screen').classList.add('active');
-	})
+	});
+
+document.querySelector('.close')
+	.addEventListener('click', function() {
+		document.querySelector('.menu-screen').classList.remove('active');
+	});
