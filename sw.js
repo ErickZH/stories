98e7c685b8ffeb4dfa8bf00ae73c54db6f8da8e8
Erick Zarate Hernandez <zaraterick@outlook.com>
2018-03-18 15:53:00 -0600

    Actualización del cache de las sw.

--- sw.js
+++ sw.js
@@ -1,8 +1,21 @@
-const CACHE_NAME = 'STORIES_CACHE-v1';
+const CACHE_NAME = 'STORIES_CACHE-v2';
 
 self.addEventListener('install', function() {
     // Guardar los archivos iniciales
     caches.open(CACHE_NAME).then(function(cache) {
-        cache.addAll(['/index.html']);
+        cache.addAll(['/index.html', '/dist/javascript/bundle.js']);
     })
+});
+
+self.addEventListener('activate', function(ev) {
+
+    ev.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            let promises = cacheNames.map(cacheName => {
+                if (CACHE_NAME !== cacheName) return caches.delete(cacheName);
+            });
+
+            return Promise.all(promises);
+        })
+    );
 });
