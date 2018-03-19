cafae1a16bd5aac11326454183ebbb1366b884fe
Erick Zarate Hernandez <zaraterick@outlook.com>
2018-03-18 21:13:30 -0600

    Envio de archivos al cache.

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'STORIES_CACHE-v2';
 self.addEventListener('install', function() {
     // Guardar los archivos iniciales
     caches.open(CACHE_NAME).then(function(cache) {
-        cache.addAll(['/index.html', '/dist/javascript/bundle.js']);
+        cache.addAll(['/index.html', '/dist/javascript/bundle.js', '/public/images/2.jpg']);
     })
 });
 
@@ -16,6 +16,16 @@ self.addEventListener('activate', function(ev) {
             });
 
             return Promise.all(promises);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(ev) {
+    ev.respondWith(
+        caches.match(ev.request)
+        .then(function(response) {
+
+            return response || fetch(ev.request);
         })
     );
 });
